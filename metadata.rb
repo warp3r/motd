b5d5544860bbb71b7b357c4d33e5a34ce25453ed
Jordi Molina <warp3r@2shifted.com>
2019-05-08 16:44:47 +0200

    Remove dependency for newer chef releases

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,6 @@ license          'GNU Public License 3.0'
 description      'Installs/Configures motd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.6.4'
-depends          'chef_handler'
 
 %w(ubuntu debian redhat centos scientific amazon fedora).each do |os|
   supports os
